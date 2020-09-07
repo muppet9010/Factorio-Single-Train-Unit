4e0410b99c739b3b34681fc69274bc368257a4ca
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-09-07 11:45:36 +0000

    WIP graphics are turned off by default

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend(
         {
             name = "single_train_unit-use_wip_graphics",
             type = "bool-setting",
-            default_value = true,
+            default_value = false,
             setting_type = "startup",
             order = "1001"
         }
