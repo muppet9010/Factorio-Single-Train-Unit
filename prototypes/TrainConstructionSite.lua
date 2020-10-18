48ebeb00947f5e818ab790509706859232eb2cdf
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-10-18 17:08:48 +0000

    Fix Space Exploration Integration not unlocking recipes.

--- prototypes/TrainConstructionSite.lua
+++ prototypes/TrainConstructionSite.lua
@@ -1,12 +1,17 @@
 local Utils = require("utility/utils")
 local StaticData = require("static-data")
 
-if not mods["space-exploration"] then
+if not mods["trainConstructionMod"] then
     return
 end
 
 -- Remove our default added recipes for Train Construction Site mod as it makes a loop and crashs game on startup otherwis.
 local effectIndex = Utils.GetTableKeyWithInnerKeyValue(data.raw["technology"]["railway"].effects, "recipe", StaticData.DoubleEndCargoPlacement.name)
-data.raw["technology"]["railway"].effects[effectIndex] = nil
+if effectIndex ~= nil then
+    data.raw["technology"]["railway"].effects[effectIndex] = nil
+end
+
 effectIndex = Utils.GetTableKeyWithInnerKeyValue(data.raw["technology"]["fluid-wagon"].effects, "recipe", StaticData.DoubleEndFluidPlacement.name)
-data.raw["technology"]["fluid-wagon"].effects[effectIndex] = nil
+if effectIndex ~= nil then
+    data.raw["technology"]["fluid-wagon"].effects[effectIndex] = nil
+end
